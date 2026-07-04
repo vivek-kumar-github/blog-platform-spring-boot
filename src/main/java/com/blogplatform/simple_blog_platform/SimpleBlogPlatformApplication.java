1c2652b3f89cdb672e63dc191e83e8d1936f0f44
Vivek Kumar <viveksingh599999@gmail.com>
2026-07-04 23:20:34 +0530

    Initial commit: Spring Boot project skeleton

---
+++ src/main/java/com/blogplatform/simple_blog_platform/SimpleBlogPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.blogplatform.simple_blog_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogplatform/simple_blog_platform/SimpleBlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogplatform.simple_blog_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
