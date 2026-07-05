1ef4f64c0d96fe50a4b513791a94a1a329d3748e
Vivek Kumar <viveksingh599999@gmail.com>
2026-07-05 13:07:00 +0530

    feat(model): add post POJO to handle post data

---
+++ src/main/java/com/blogplatform/simple_blog_platform/model/Post.java
@@ -0,0 +1,49 @@
+package com.blogplatform.simple_blog_platform.model;
+
+import java.time.LocalDateTime;
+
+public class Post {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    public Post(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
