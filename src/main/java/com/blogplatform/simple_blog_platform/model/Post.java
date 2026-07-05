30326951f43a93f7acb483adfb9fb6df92eb6067
Vivek Kumar <viveksingh599999@gmail.com>
2026-07-05 22:31:43 +0530

    chore: add annotations in Post class

--- src/main/java/com/blogplatform/simple_blog_platform/model/Post.java
+++ src/main/java/com/blogplatform/simple_blog_platform/model/Post.java
@@ -1,9 +1,17 @@
 package com.blogplatform.simple_blog_platform.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 
+@Entity
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
