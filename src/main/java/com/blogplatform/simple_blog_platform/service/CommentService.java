b4bd743564367c2b6566e5b73acc0c87d1a159ef
Vivek Kumar <viveksingh599999@gmail.com>
2026-07-13 00:58:10 +0530

    feat(service): implement saveComment method to create and save comments

--- src/main/java/com/blogplatform/simple_blog_platform/service/CommentService.java
+++ src/main/java/com/blogplatform/simple_blog_platform/service/CommentService.java
@@ -1,7 +1,23 @@
 package com.blogplatform.simple_blog_platform.service;
 
+import com.blogplatform.simple_blog_platform.repository.CommentRepository;
 import org.springframework.stereotype.Service;
+import com.blogplatform.simple_blog_platform.model.Comment;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public Comment saveComment(Comment comment) {
+        comment.setCreatedAt(LocalDateTime.now());
+
+        return commentRepository.save(comment);
+    }
 }
