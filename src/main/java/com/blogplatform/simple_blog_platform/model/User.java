a6e7009101a9e1602f13f0c0ea64f2f3c76aa957
Vivek Kumar <viveksingh599999@gmail.com>
2026-07-16 09:54:34 +0530

    feat(model): update User entity with proper annotations and formatting

--- src/main/java/com/blogplatform/simple_blog_platform/model/User.java
+++ src/main/java/com/blogplatform/simple_blog_platform/model/User.java
@@ -1,22 +1,31 @@
 package com.blogplatform.simple_blog_platform.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments;
 
     public User() {
 
@@ -52,5 +61,21 @@ public class User {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
     }
 }
