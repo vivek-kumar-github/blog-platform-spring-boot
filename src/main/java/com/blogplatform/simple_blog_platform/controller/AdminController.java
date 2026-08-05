b2269a07de0d72e248511bcfde14efeb01ee0dfb
Vivek Kumar <viveksingh599999@gmail.com>
2026-08-06 00:13:06 +0530

    feat(controller): add showEditPostForm method to AdminController for editing posts

--- src/main/java/com/blogplatform/simple_blog_platform/controller/AdminController.java
+++ src/main/java/com/blogplatform/simple_blog_platform/controller/AdminController.java
@@ -4,10 +4,7 @@ import com.blogplatform.simple_blog_platform.model.Post;
 import com.blogplatform.simple_blog_platform.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -50,6 +47,16 @@ public class AdminController {
         postService.savePost(post, username);
 
         return "redirect:/admin/posts";
+    }
+
+    @GetMapping("/posts/edit/{id}")
+    public String showEditPostForm(@PathVariable Long id, Model model) {
+
+        Post post = postService.findPostById(id);
+
+        model.addAttribute("post", post);
+
+        return "admin/post-form";
     }
 
 }
