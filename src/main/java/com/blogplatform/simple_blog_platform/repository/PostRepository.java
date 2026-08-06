36bf2d582ab580f9873d0e693a4f0203d5a443c9
Vivek Kumar <viveksingh599999@gmail.com>
2026-08-06 21:59:45 +0530

    feat(search): implement search functionality in home page for filtering posts by title

--- src/main/java/com/blogplatform/simple_blog_platform/repository/PostRepository.java
+++ src/main/java/com/blogplatform/simple_blog_platform/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.blogplatform.simple_blog_platform.repository;
 
 import com.blogplatform.simple_blog_platform.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Page<Post> findByTitleContainingIgnoreCase(String keyword, Pageable pageable);
 }
